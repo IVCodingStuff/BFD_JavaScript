9eca9adf011ee5cfdf4775ed51f0c50298dadfdf
IV <60792969+IVCodingStuff@users.noreply.github.com>
2025-08-12 16:42:05 -0300

    aula 3

---
+++ Curso Front End/Exercicio_3/script.js
@@ -0,0 +1,5 @@
+function mostrarSaudacao(){
+    let nome = "viajante"
+    const saudacao = document.getElementById("mensagem");
+    mensagem.textContent = "Olá, "+ nome + ", pronto pra navegar?";
+}
---
+++ Curso Front End/Exercicio_4/script.js
@@ -0,0 +1,4 @@
+function mostrarApresentacao(){
+const apresentacao = document.getElementById("mensagem")
+mensagem.textContent = "Opa! Meu nome é Isa, tenho 20 anos e estou estudando tanta coisa que nem sei por onde começar. Meus hobbies incluem desenhar e cantar, além de jogar um bom e velho Tetris."
+}
